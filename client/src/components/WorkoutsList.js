ea0510536afcee5ea483f4cb475d0065978af1a3
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2023-11-08 15:37:14 +0100

    fix: correctly redirct to workoutpage

--- client/src/components/WorkoutsList.js
+++ client/src/components/WorkoutsList.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 function WorkoutsList() {
 
     const [workouts, setWorkouts] = useState([])
+    const navigate = useNavigate()
 
     const getWorkouts = async () => {
         try {
@@ -18,8 +20,6 @@ function WorkoutsList() {
         getWorkouts()
     }, [])
 
-    console.log(workouts[0])
-
     return (
         <div>
             <button>Log new workout</button>
@@ -27,7 +27,9 @@ function WorkoutsList() {
                 <h4>Workouts</h4>
                 {workouts && workouts.map(workout => (
                     <li key={workout.workout_id}>
-                        {Intl.DateTimeFormat('en-GB', { dateStyle: "long"}).format(new Date(workout.workout_date))}
+                        <button onClick={() => navigate(`/workout/${workout.workout_id}`)}>
+                            {Intl.DateTimeFormat('en-GB', { dateStyle: "long"}).format(new Date(workout.workout_date))}
+                        </button>
                     </li>
                 ))}
             </ul>
