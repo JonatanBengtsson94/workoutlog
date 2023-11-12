c6583602747d72ad798c482fc8844cf7cf467dd9
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2023-11-12 20:32:44 +0100

    feat: setup session storage

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const session = require("express-session")
+const pg = require("pg")
 const exercises = require("./routes/exercises")
 const sets = require("./routes/sets")
 const workouts = require("./routes/workouts")
 
+const pgPool = new pg.Pool()
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.use(session({
+    store: new (require("connect-pg-simple")(session))({
+        pool: pgPool,
+        createTableIfMissing: true
+    }),
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
 
 // Exercises
 app.use("/api/v1/exercises", exercises)
