0c7c33607d3c9a709022dac31882882809528531
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2023-11-13 18:09:19 +0100

    feat: created route for login

---
+++ client/src/routes/Login.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+
+function Login() {
+    return (
+        <div className="container">
+            <Header />
+            <main>
+            </main>
+        </div>
+    )
+}
+
+export default Login
