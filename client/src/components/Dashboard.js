dd1345aeefc777c64a6dbaf09672f699f279b68b
jonatan <jonatan.bengtsson1994@gmail.com>
2023-11-25 14:54:16 +0100

    feat: added dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+function Dashboard() {
+    return (
+        <div className="main-container">
+            <div className="benchpress-div"></div>
+            <div className="deadlift-div"></div>
+            <div clasName="squat-div"></div>
+        </div>
+    )
+}
+
+export default Dashboard
