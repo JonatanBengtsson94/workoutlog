594b4de5f9cdaef49c81e424c3e1f69892c34f01
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-05-31 18:35:13 +0200

    feat: added component grouping sets by exercise

---
+++ client/src/components/ExerciseForm.js
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+function ExerciseForm({ exercise }) {
+
+    const [sets, setSets] = useState([{ reps: "", weight: "" }])
+
+    const handleAddSet = () => {
+        setSets([...sets, { reps: "", weight: "" }])
+    }
+
+    const handleRemoveSet = (e, index) => {
+        e.preventDefault()
+        setSets(prevState => {
+            const updatedSets = [...prevState]
+            updatedSets.splice(index, 1)
+            return updatedSets
+        })
+    }
+
+    const handleChange = (index, key, value) => {
+        setSets(prevState => {
+            const updatedSets = [...prevState]
+            updatedSets[index][key] = value
+            return updatedSets
+        })
+    }
+
+    return (
+        <div className="main-container">
+            <h4 className="sub-title">{exercise}</h4>
+            <div className="exercise-container">
+                <h5 className="header">Reps</h5>
+                <h5 className="header">Weight</h5>
+                <h5 className="header"></h5>
+                {sets.map((set, index) => (
+                    <div key={index} className="set-input-div">
+                        <input
+                            type="text"
+                            id="reps"
+                            value={set.reps}
+                            onChange={e => handleChange(index, "reps", e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            id="weight"
+                            value={set.weight}
+                            onChange={e => handleChange(index, "weight", e.target.value)}
+                        />
+                        <button className="cancel-btn" onClick={e => handleRemoveSet(e, index)}>X</button>
+                    </div>
+                ))}
+            </div>
+            <button className="confirm-btn" onClick={handleAddSet}>Add set</button>
+        </div>
+    )
+}
+
+export default ExerciseForm
